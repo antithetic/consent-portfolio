d10327091a16fd7c491895a096397198daa63c10
Raúl Garcia <ego.war@icloud.com>
2025-11-10 14:08:14 -0800

    🎉 Astro initial setup
    re-installed Astroo

---
+++ apps/frontend/.prettierrc.mjs
@@ -0,0 +1,17 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: ['**/*.astro'],
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
