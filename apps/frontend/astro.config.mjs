f49fb8478bdfeb9e29f647b244997ecfa1493d32
Raúl Garcia <ego.war@icloud.com>
2025-11-10 22:54:52 -0800

    ✨ Markdoc
    add markdoc functionality to astro

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -3,7 +3,9 @@ import mdx from '@astrojs/mdx'
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
+import markdoc from '@astrojs/markdoc';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), UnoCSS({})],
+  integrations: [mdx(), UnoCSS({}), markdoc()],
 })
